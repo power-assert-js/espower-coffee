9c159205608e6a556f61167d1fb65123ae2421ab
Takuto Wada <takuto.wada@gmail.com>
2014-09-02 17:04:01 +0900

    feat(espower-coffee): interact with coffeescript compiler to adjust line number in power-assert output

--- index.js
+++ index.js
@@ -8,6 +8,8 @@
 var coffee = require('coffee-script'),
     originalCompileFile = coffee._compileFile,
     minimatch = require('minimatch'),
+    extend = require('xtend'),
+    convert = require('convert-source-map'),
     espowerSource = require('espower-source');
 
 function espowerCoffee (options) {
@@ -17,15 +19,18 @@ function espowerCoffee (options) {
         pattern = options.cwd + separator + options.pattern;
 
     coffee._compileFile = function (filepath, sourceMap) {
-        var answer = originalCompileFile(filepath, sourceMap);
+        var withMap = originalCompileFile(filepath, true); // enable sourcemaps
         if (minimatch(filepath, pattern)){
-            if (sourceMap) {
-                answer.js = espowerSource(answer.js, filepath, options.espowerOptions);
-            } else {
-                answer = espowerSource(answer, filepath, options.espowerOptions);
-            }
+            var conv = convert.fromJSON(withMap.v3SourceMap);
+            // restore filepath since coffeescript compiler drops it
+            conv.setProperty('sources', [filepath]);
+            withMap.js = espowerSource(
+                withMap.js,
+                filepath,
+                extend(options.espowerOptions, {sourceMap: conv.toObject()})
+            );
         }
-        return answer;
+        return sourceMap ? withMap : withMap.js;
     };
 
     coffee.register();
